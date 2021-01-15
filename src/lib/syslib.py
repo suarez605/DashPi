db7a06ae0b27b7dd2c04459b9f29aa69ff0e6710
suarez605 <alvaro.suarez2212@gmail.com>
2021-01-16 00:52:47 +0100

    mas

--- src/lib/syslib.py
+++ src/lib/syslib.py
@@ -2,14 +2,14 @@ from subprocess import PIPE, Popen
 import os
 
 #CPU Temp
-async def get_cpu_temperature():
+def get_cpu_temperature():
     """get cpu temperature using vcgencmd"""
     process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
     output, _error = process.communicate()
-    return float(output[output.index('=') + 1:output.rindex("'")])
+    return str(output[output.index('=') + 1:output.rindex("'")])
 
 #Memory Information
-async def get_ram():
+def get_ram():
     free = os.popen("free -h")
     line = free.readline()
     description = line.split()[0:7]
@@ -20,4 +20,4 @@ async def get_ram():
     for desc in description:
         result[desc] = data[count]
         count += 1
-    return result
+    return str(result)

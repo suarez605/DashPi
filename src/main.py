681e0e160f459f37ca75748f8d13846eb1066d11
suarez605 <alvaro.suarez2212@gmail.com>
2021-01-16 01:08:32 +0100

    added flask basic endpoint

--- src/main.py
+++ src/main.py
@@ -2,14 +2,28 @@
 from src.lib import gathering as dg
 import sys
 import threading
+from flask import Flask
+
+
+freq = sys.argv[1]
+data_recovery_object = dg.DataGathering(freq)
 
 
 def data_recovery(freq: float):
-    data_recovery_object = dg.DataGathering(freq)
+    global data_recovery_object
     data_recovery_object.retrieve_data()
-   
+
+
+app = Flask()
+
+
+@app.route('/', )
+def get_data():
+    return f'CPU: {str(data_recovery_object.temp_list)} C  |  MEM: {str(data_recovery_object.ram_list)}'
 
 if __name__ == "__main__":
-    freq = sys.argv[1]
-    x = threading.Thread(target=data_recovery, args=(freq,))
+    x = threading.Thread(target=data_recovery, args=(freq,), daemon=True)
     x.start()
+    app.run(host='127.0.0.1', port=80)
+
+
